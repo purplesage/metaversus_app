8a860922c6540494b34093c7f2b0c023ec797a5e
david <hectormza21@gmail.com>
2022-12-11 19:23:43 -0400

    get started and new features section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,14 +1,16 @@
-"use client";
+/* eslint-disable react/jsx-wrap-multilines */
 
-import { useState } from "react";
-import { motion } from "framer-motion";
-import { TypingText, ExploreCard, TitleText } from "../components";
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { exploreWorlds } from "../constants";
+'use client';
+
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { TypingText, ExploreCard, TitleText } from '../components';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '../constants';
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2');
 
   return (
     <section className={`${styles.paddings}`} id="explore">
