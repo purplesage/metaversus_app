85e687981ee76e9c4925fc47af74664b2cd06cc6
purpleSage <hectormza21@gmail.com>
2022-12-16 13:33:24 -0400

    ...

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,31 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row  gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[350px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Samantha
+          </h4>
+          <p>Founder | Metaversus</p>
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
