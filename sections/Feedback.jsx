e6152407b222b90dac6b090217a56ed48bc229c6
purpleSage <hectormza21@gmail.com>
2022-12-23 15:31:30 -0400

    added feedback section

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,11 +19,38 @@ const Feedback = () => (
       >
         <div className="feedback-gradient" />
         <div>
-          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+          <h4 className="font-bold sm:text-[32px] text-[32px] sm:leading-[40px] leading-[36px] text-white">
             Samantha
           </h4>
-          <p>Founder | Metaversus</p>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
         </div>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+          "With the development of today's technology, metaverse is very useful
+          for today's work, or can be called web 3.0. by using metaverse you can
+          use it as anything".
+        </p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/planet-09.png"
+          alt="planet-09"
+          className="w-full lg:[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%] "
+        >
+          <img
+            src="/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain"
+          />
+        </motion.div>
       </motion.div>
     </motion.div>
   </section>
